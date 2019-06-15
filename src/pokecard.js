1663842072470e4706ada8c07f1e8b49080db2e8
Joe <joe@astucc.io>
2019-06-15 15:28:25 -0400

    initial commit

---
+++ src/pokecard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import deck from './pokedex'
+
+export default function Pokecard() {
+  return (
+    <>
+      <h2>name</h2>
+      <div>img</div>
+      <p>type:</p>
+      <p>exp:</p>
+    </>
+  )
+}

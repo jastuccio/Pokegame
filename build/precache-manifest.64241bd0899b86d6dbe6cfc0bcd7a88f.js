585be5804c4e21a3a0f3be152d1521286a559fdf
Joe <joe@astucc.io>
2019-06-27 16:17:20 -0400

    chore: commit build folder

---
+++ build/precache-manifest.64241bd0899b86d6dbe6cfc0bcd7a88f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e0a1600b168b34a82308d7c8ec3c95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5acd98997b430993fcba",
+    "url": "/static/css/main.f0cad951.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "5acd98997b430993fcba",
+    "url": "/static/js/main.b5d3a80d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
